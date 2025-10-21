83c0a4603ac5e11bf704e7c58d98c076d2440aad
Rebeca D.Montenegro <rebediazmontenegro@gmail.com>
2025-10-21 21:57:48 +0200

    Done SUp/SIn/SOut

--- js/script.js
+++ js/script.js
@@ -1,5 +1,196 @@
-// ------------------------------------------ MAPA 1 ------------------------------------------
+const firebaseConfig = {
+    apiKey: "AIzaSyCxDmYsOau4FBT0PfobJ_1jevkQPVk1XQ8",
+    authDomain: "fir-web-589d4.firebaseapp.com",
+    projectId: "fir-web-589d4",
+    storageBucket: "fir-web-589d4.firebasestorage.app",
+    messagingSenderId: "674609037037",
+    appId: "1:674609037037:web:f1a54da2f2f49de812d6e4"
+  }; // Son los datos de conexion, con esto accedes al firebase
 
+firebase.initializeApp(firebaseConfig);// Inicializaar app Firebase
+
+const db = firebase.firestore();// db representa mi base de datos //inicia Firestore
+
+// -------------------------------------------------------------------------------------------------------------------------------
+
+// Función para guardar usuarios en Firestore y que se cree la colección
+
+function createUser(usuario) {
+  db.collection("users-terremotos")
+    .add(usuario)
+    .then((docRef) => {
+      console.log("Usuario guardado con ID:", docRef.id);
+    })
+    .catch((error) => {
+      console.error("Error al guardar el usuario:", error);
+      });
+    }
+
+// -------------------------------------------------------------------------------------------------------------------------------
+
+// SIGN UP
+
+const signUpUser = (email, password, name) => {
+  firebase.auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(`Se ha registrado ${user.email}`);
+        Swal.fire(`Se ha registrado ${user.email}`);
+
+      // Guardar en Firestore con nombre
+      createUser({
+        id: user.uid,
+        email: user.email,
+        name: name
+      });
+    })
+    .catch((error) => {
+      console.log("Error en el sistema: " + error.message, "Error: " + error.code);
+      alert("Error: " + error.message);
+    });
+};
+
+
+
+// -------------------------------------------------------------------------------------------------------------------------------
+
+// Crear formulario cuando le das al boton de crear o login -----------------------------------------------------------------------
+document.addEventListener("DOMContentLoaded", () => {
+  const contenedor = document.getElementById("contenedor-form");
+
+  document.getElementById("registrar").addEventListener("click", () => {
+    contenedor.innerHTML = `
+      <p>Crea una cuenta</p>
+      <form class="registroUsuario">
+        <label>Nombre de usuario</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Introduce tu nombre de usuario"
+          required
+        />
+        <label>Correo electrónico</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Introduce tu correo electrónico" 
+          required
+        />
+        <label>Password:</label>
+        <input 
+          type="password"
+          id="pass"
+          name="pass"
+          placeholder="Introduce password..."
+          required
+        />
+        <label>Repite password:</label>
+        <input
+          type="password"
+          id="pass2"
+          name="pass2"
+          placeholder="Repite password..."
+          required
+        />
+        <button class="botonCrear" type="submit">Crear</button>
+      </form>
+    `;
+  });
+
+  document.getElementById("login").addEventListener("click", () => {
+    contenedor.innerHTML = `
+      <p>Log In</p>
+      <form class="loginUser">
+        <label>Correo electrónico</label>
+        <input
+          type="email"
+          id="emailLog"
+          name="emailLog"
+          placeholder="Introduce tu correo electrónico"
+          required
+        />
+        <label>Contraseña</label>
+        <input
+          type="password"
+          id="passwordLog"
+          name="passwordLog"
+          placeholder="Introduce tu contraseña"
+          required
+        />
+        <button class="botonEnviar" type="submit">Enviar</button>
+      </form>
+    `;
+  });
+
+  contenedor.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // Formulario de registro
+    if (event.target.classList.contains("registroUsuario")) { 
+      const name = event.target.elements.name.value;
+      const email = event.target.elements.email.value;
+      const pass = event.target.elements.pass.value;
+      const pass2 = event.target.elements.pass2.value;
+    
+      if (pass === pass2 && pass.length >= 6) { 
+        signUpUser(email, pass, name);
+        contenedor.innerHTML = "";
+
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Contraseña no válida",
+          text: "La contraseña debe tener al menos 6 dígitos",
+        });
+      }
+    }
+
+    // Formulario de login
+    if (event.target.classList.contains("loginUser")) { 
+      const email = event.target.elements.emailLog.value;
+      const password = event.target.elements.passwordLog.value;
+
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(`Sesión iniciada: ${user.email}`);
+          Swal.fire(`Sesión iniciada: ${user.email}`);
+
+          contenedor.innerHTML = `
+            <aside class="bienvenida">
+            <h3>¡Bienvenido, ${user.email}!</h3>
+            <button id="botonLogOut" type="submit">Desconectar</button>
+            </aside>
+          `;
+
+          // Cerrar sesion
+          document.getElementById("botonLogOut").addEventListener("click", () => {
+            firebase.auth().signOut()
+              .then(() => {
+                console.log(`Sesión cerrada ${user.email}`);
+                Swal.fire("Sesión cerrada correctamente");
+                contenedor.innerHTML = "";
+              })
+              .catch((error) => {
+                Swal.fire("Error al cerrar sesión: " + error.message);
+              });
+          });
+        })
+        .catch((error) => {
+          Swal.fire("Error de login: " + error.message);
+        });
+    }
+  });
+});
+
+
+
+// -------------------------------------------------------------------------------------------------------------------------------
+
+// ------------------------------------------ MAPA 1 ------------------------------------------
 let map = L.map('map').setView([20, 0], 2);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -66,6 +257,7 @@ getData().then(data => {
         marcador.on("popupopen", () => {
             const favBoton = document.querySelector('.favBoton');
             if(favBoton){
+                //Logica para añadir favoritos al objeto del usuario
                 favBoton.addEventListener("click", () => {
                     alert("favorito guardado");
                 })
@@ -76,7 +268,7 @@ getData().then(data => {
 
 
 
-// ------------------------------------------ MAPA 1 ------------------------------------------
+// ------------------------------------------ MAPA 2 ------------------------------------------
 
 let mapTwo = L.map('mapTwo').setView([20, 0], 2);
 
@@ -188,6 +380,5 @@ filtroFecha.addEventListener('submit', (e) => {
     e.preventDefault();
     aplicarFiltros();
 });
-
 
 getData().then(data => mostrarTerremotos(data));
